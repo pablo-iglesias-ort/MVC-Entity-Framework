a7105fa3ff4a9880da7439caa16419a99cdae18b
pablo-iglesias-ort <pablo.iglesias@ort.edu.ar>
2021-09-12 17:33:24 -0300

    CRUD de materias

---
+++ MVC-Entity-Framework/Controllers/MateriasController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC_Entity_Framework.Data;
+using MVC_Entity_Framework.Models;
+
+namespace MVC_Entity_Framework.Controllers
+{
+    public class MateriasController : Controller
+    {
+        private readonly MVC_Entity_FrameworkContext _context;
+
+        public MateriasController(MVC_Entity_FrameworkContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Materias
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Materias.ToListAsync());
+        }
+
+        // GET: Materias/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var materia = await _context.Materias
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (materia == null)
+            {
+                return NotFound();
+            }
+
+            return View(materia);
+        }
+
+        // GET: Materias/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Materias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre")] Materia materia)
+        {
+            if (ModelState.IsValid)
+            {
+                materia.Id = Guid.NewGuid();
+                _context.Add(materia);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(materia);
+        }
+
+        // GET: Materias/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var materia = await _context.Materias.FindAsync(id);
+            if (materia == null)
+            {
+                return NotFound();
+            }
+            return View(materia);
+        }
+
+        // POST: Materias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Nombre")] Materia materia)
+        {
+            if (id != materia.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(materia);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MateriaExists(materia.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(materia);
+        }
+
+        // GET: Materias/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var materia = await _context.Materias
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (materia == null)
+            {
+                return NotFound();
+            }
+
+            return View(materia);
+        }
+
+        // POST: Materias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var materia = await _context.Materias.FindAsync(id);
+            _context.Materias.Remove(materia);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MateriaExists(Guid id)
+        {
+            return _context.Materias.Any(e => e.Id == id);
+        }
+    }
+}
